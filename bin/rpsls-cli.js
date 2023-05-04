791acf5bc1b6452e10a97da897a7ba38fc2b20c7
Wesley Adam Hall <ha0406@ad.unc.edu>
2023-05-03 21:33:38 -0400

    Create and fill rpsls-cli.js

---
+++ bin/rpsls-cli.js
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+import { rps } from ".../lib/rpsls.js"
+import minimist from "minimist";
+
+const args = minimist(process.argv.slice(2));
+
+// -r, --r rules
+if (args.r || args.rules) {
+  console.log(`Rules for the Lizard-Spock Expansion of Rock Paper Scissors:
+
+  - Scissors CUTS Paper
+  - Paper COVERS Rock
+  - Rock SMOOSHES Lizard
+  - Lizard POISONS Spock
+  - Spock SMASHES Scissors
+  - Scissors DECAPITATES Lizard
+  - Lizard EATS Paper
+  - Paper DISPROVES Spock
+  - Spock VAPORIZES Rock
+  - Rock CRUSHES Scissors`);
+  process.exit(0);
+}
+
+// -h, --h help
+if (args.h || args.help) {
+  console.log(`Usage: node-rpsls [SHOT]
+  Play the Lizard-Spock Expansion of Rock Paper Scissors (RPSLS)!
+
+    -h, --help        display this help message and exit
+    -r, --rules       display the rules and exit
+
+  Examples:
+    node-rpsls        Return JSON with single player RPSLS result.
+                      e.g. {"player":"rock"}
+    node-rpsls rock   Return JSON with results for RPSLS played against a simulated opponent.
+                      e.g {"player":"rock","opponent":"Spock","result":"lose"}`);
+  process.exit(0);
+}
+
+var results = args._[0];
+console.log(JSON.stringify(rpsls(results)));
+process.exit(0);
