0e5102b26eef103a4bc4987f664d52c7ea4f5e7c
Wesley Adam Hall <ha0406@ad.unc.edu>
2023-05-03 21:25:55 -0400

    Create and fill rps-cli.js

---
+++ bin/rps-cli.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+import { rps } from ".../lib/rpsls.js"
+import minimist from "minimist";
+
+const args = minimist(process.argv.slice(2));
+
+// -r, --r rules
+if (args.r || args.rules) {
+  console.log(`Rules for Rock Paper Scissors:
+
+  - Scissors CUTS Paper
+  - Paper COVERS Rock
+  - Rock CRUSHES Scissors`);
+}
+
+// -h, --h help
+if (args.h || args.help) {
+  console.log(`Usage: node-rps [SHOT]
+  Play Rock Paper Scissors (RPS)
+
+    -h, --help      display this help message and exit
+    -r, --rules     display the rules and exit
+
+  Examples:
+    node-rps        Return JSON with single player RPS result.
+                    e.g. {"player":"rock"}
+    node-rps rock   Return JSON with results for RPS played against a simulated opponent.
+                    e.g {"player":"rock","opponent":"scissors","result":"win"}`);
+  process.exit(0);
+}
+
+var results = args._[0];
+
+console.log(JSON.stringify(rps(results)));
+process.exit(0);
